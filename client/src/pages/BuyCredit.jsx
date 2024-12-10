2adcc496ab530640df161b93a051a184c7fb1004
Kirti Lohchab <ahlawatkirti227@gmail.com>
2024-12-10 12:58:54 +0530

    toast is defined

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -5,6 +5,7 @@ import { AppContext } from "./../context/AppContext";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@clerk/clerk-react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const BuyCredit = () => {
   const { backendUrl, loadCreditsData } = useContext(AppContext);
